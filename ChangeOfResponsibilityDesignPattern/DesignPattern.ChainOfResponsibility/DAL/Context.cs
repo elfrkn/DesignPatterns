64f490db8f78b08b14b51da6b77a095be4ba0f1c
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-05-17 13:25:23 +0200

    Add project files.

---
+++ ChangeOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibility.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;initial catalog=DesigPatternCOR;integrated security=true;");
+        }
+        public  DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}

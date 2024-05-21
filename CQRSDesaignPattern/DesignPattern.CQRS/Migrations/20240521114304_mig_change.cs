6abee42ffdc5be0f92d2f09a04a9181468013e35
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-05-21 14:42:40 +0200

    ObserverDesignPattern

---
+++ CQRSDesaignPattern/DesignPattern.CQRS/Migrations/20240521114304_mig_change.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignPattern.CQRS.Migrations
+{
+    public partial class mig_change : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ObserverDesignPattern/DesignPattern.Observer/Migrations/20240521113900_mig_change_name.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignPattern.Observer.Migrations
+{
+    public partial class mig_change_name : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

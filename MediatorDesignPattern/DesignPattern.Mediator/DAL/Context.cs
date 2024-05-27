a3a903283246f3a1c48b27b6edd52c1938db06e4
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-05-27 18:58:48 +0200

    MediatR Design Pattern

---
+++ MediatorDesignPattern/DesignPattern.Mediator/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Mediator.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;initial catalog=DesigPatternMediator;integrated security=true;");
+        }
+        public  DbSet<Product> Products { get; set; }
+    }
+}
